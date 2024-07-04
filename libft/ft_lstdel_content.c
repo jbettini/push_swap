70b4075c0a6bfed51888999dc5594dd5060ad4e9
jbettini <ashhxtm@gmail.com>
2024-07-04 02:08:31 +0200

    fix

--- libft/ft_lstdel_content.c
+++ libft/ft_lstdel_content.c
@@ -6,7 +6,7 @@
 /*   By: jbettini <jbettini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 18:11:18 by jbettini          #+#    #+#             */
-/*   Updated: 2021/12/16 16:41:48 by jbettini         ###   ########.fr       */
+/*   Updated: 2024/07/04 02:07:22 by jbettini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,4 +15,5 @@
 void	ft_lstdel_content(void *content)
 {
 	content = NULL;
+	(void)content;
 }
